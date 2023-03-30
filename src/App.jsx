e8b1c354baada83e490fec9fb84e75f7f9cc44d4
Vincent Liussyaputra <liusvinv@gmail.com>
2023-03-30 23:54:57 +0700

    Add the basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <div className="App">
-      
+      <p>20:13:55</p>
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
     </div>
   )
 }
