8a321216664a4e2ceb8b4ad47f15e3826254b64f
Vincent_Liussyaputra <liusvinv@gmail.com>
2023-07-10 18:39:10 +0700

    Add persistent to saved location with local storage

--- src/components/LocationSearch.jsx
+++ src/components/LocationSearch.jsx
@@ -1,245 +1,271 @@
 import axios from "axios";
-import { useRef, useState } from "react"
+import { useRef, useState } from "react";
 import moment from "moment-timezone";
 import styled from "styled-components";
 import StyledButton from "./StyledButton";
 import StyledModalForm from "./StyledModalForm";
 
-import {FaArrowRight, FaLocationArrow, FaSearchLocation} from 'react-icons/fa'
-import {TiLocation} from 'react-icons/ti'
+import {
+  FaArrowRight,
+  FaLocationArrow,
+  FaSearchLocation,
+} from "react-icons/fa";
+import { TiLocation } from "react-icons/ti";
 import { useQuery } from "react-query";
 
 const StyledLocationSearch = styled.div`
-    background-color: #b3ddf75f;
-    margin: 0 .6em;
+  background-color: #b3ddf75f;
+  margin: 0 0.6em;
+  border-radius: 1em;
+  padding: 2.4em;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  resize: horizontal;
+  overflow: hidden;
+  min-width: 350px;
+  height: 58vh;
+
+  h2 {
+    opacity: 0.8;
+  }
+  & .location-res {
+    background-color: #ffffff8c;
+    padding: 1em;
+    flex-grow: 1;
     border-radius: 1em;
-    padding: 2.4em;
-    width:400px;
-    display: flex;
-    flex-direction: column;
-    resize: horizontal;
+    min-width: fit-content;
+    height: fit-content;
+    max-width: 100%;
+    box-shadow: 0px 2px 4px #757ce74e;
+    transition: box-shadow 250ms ease;
+  }
+  & .location-res:hover {
+    box-shadow: 0px 4px 4px #5056a352;
+  }
+
+  input {
+    border-radius: 10px;
+  }
+  & .location-res p {
+    font-size: 0.7rem;
+  }
+  & .location-res hr {
+    opacity: 0.4;
+    color: #9466a1;
+    margin: 0.21em 0;
+  }
+  & .location-res h2 {
+    font-size: 1.2rem;
+    color: #9466a1;
+    font-weight: normal;
+    white-space: nowrap;
     overflow: hidden;
-    min-width: 350px;
-    height: 58vh;
+    text-overflow: ellipsis;
+    max-width: 250px; /* adjust to your desired length */
+  }
+  .truncate {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 300px; /* adjust to your desired length */
+  }
 
-    h2{
-        opacity: .8;
-    }
-    & .location-res{
-        background-color: #ffffff8c;
-        padding: 1em;
-        flex-grow: 1;
-        border-radius: 1em;
-        min-width: fit-content;
-        height: fit-content;
-        max-width: 100%;
-        box-shadow: 0px 2px 4px #757ce74e;
-        transition: box-shadow 250ms ease;
-    }
-    & .location-res:hover{
-        box-shadow: 0px 4px 4px #5056a352;
+  & .location-res button {
+    margin-block: 1em;
+    vertical-align: baseline;
+    display: flex;
+    align-items: center;
+  }
 
-    }
+  & .location-res .action {
+    display: flex;
+    justify-content: flex-end;
+  }
+  & .location-res button svg {
+    margin: 0.4em 0.4em;
+  }
 
-    input{
-        border-radius: 10px;
-    }
-    & .location-res p{
-        font-size: .7rem;
-    }
-    & .location-res hr{
-        opacity: .4;
-        color:#9466a1;
-        margin: .21em 0;
-    }
-    & .location-res h2{
-        font-size: 1.2rem;
-        color: #9466a1;
-        font-weight: normal;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        max-width: 250px; /* adjust to your desired length */
-    }
-    .truncate {
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        max-width: 300px; /* adjust to your desired length */
-    }
+  & .location-res .detail_coord {
+    background-color: #8383ca2a;
+    padding: 0.2em;
+    text-align: center;
+    color: #3a3a3aae;
+  }
 
-    & .location-res button{
-        margin-block: 1em;
-        vertical-align: baseline;
-        display: flex;
-        align-items: center;
-    }
+  & .location-res .detail_timezone {
+    background-color: #c5c5e32a;
+    padding: 0.2em;
+    text-align: center;
+    display: flex;
+    justify-content: space-around;
+    border-radius: 5px;
+    padding: 1em;
+    color: #be6262c0;
+    font-size: 0.5rem;
+    flex-wrap: wrap;
+  }
+  & .location-res .detail_date {
+    padding: 0.2em;
+    text-align: center;
+    color: #3a3a3a93;
+    font-size: rem;
+  }
+  & .search-res {
+    margin: 0.2em;
+    display: flex;
+    justify-content: start;
+    flex-wrap: wrap;
+    gap: 2em;
+    white-space: nowrap;
+    overflow: auto;
+    padding: 0.8em;
+    padding-bottom: 1.4em;
+    box-shadow: 0px 0px 5px #50505042;
+    background-color: #7e777a1d;
+    max-height: 750px;
+    width: fit-content;
+    min-width: 100%;
+    height: 100%;
+  }
+`;
+export default function LocationSearch({ addLocation }) {
+  const searchQuery = useRef();
+  const [searchResult, setSearchResult] = useState([]);
 
-    & .location-res .action{
-        display: flex;
-        justify-content: flex-end;
-    }
-    & .location-res button svg{
-        margin: .4em .4em;
-    }
+  const [searchParam, setSearchParam] = useState("");
+  const { isLoading, refetch, isError, error } = useQuery(
+    ["searchRes", searchParam],
+    fetchLocation,
+    { enabled: false, onSuccess: (data) => manageSearchResult(data.data) }
+  );
 
-    & .location-res .detail_coord{
-        background-color: #8383ca2a;
-        padding: .2em;
-        text-align: center;
-        color: #3a3a3aae;
-    }
+  // Fetch the API for location coordinate
+  async function searchLocation(e) {
+    // Validation And Initialization
+    e.preventDefault();
+    const query = searchQuery.current.value;
+    setSearchResult([]);
+    if (query === "") return;
+    await setSearchParam(query);
+    refetch();
+    // if(queryRes.status === 200){
+    //     console.log(queryRes.data);
+    //     // manageSearchResult(queryRes.data.data);
+    // }else{
+    //     //Show Error
+    // }
+  }
+  async function fetchLocation(queryKey, id) {
+    // console.log('ID:',id);
+    console.log("QK:", queryKey.queryKey[1]);
+    // Construc the endpoint
+    const url = new URL("https://toki-timer-be.vercel.app/search");
+    url.searchParams.append("name", searchParam);
+    const queryRes = await axios.get(url);
+    return queryRes.data;
+  }
 
-    & .location-res .detail_timezone{
-        background-color: #c5c5e32a;
-        padding: .2em;
-        text-align: center;
-        display: flex;
-        justify-content: space-around;
-        border-radius: 5px;
-        padding: 1em;
-        color: #be6262c0;
-        font-size: .5rem ;
-        flex-wrap: wrap;
-    }
-    & .location-res .detail_date{
-        padding: .2em;
-        text-align: center;
-        color: #3a3a3a93;
-        font-size: rem;
-    }
-    & .search-res{
-        margin: .2em;
-        display: flex;
-        justify-content: start;
-        flex-wrap: wrap;
-        gap: 2em;
-        white-space: nowrap;
-        overflow:auto;
-        padding: .8em;
-        padding-bottom: 1.4em;
-        box-shadow: 0px 0px 5px #50505042;
-        background-color: #7e777a1d;
-        max-height: 750px;
-        width: fit-content ;
-        min-width: 100%;
-        height: 100%;
-    }
-`
-export default function LocationSearch({addLocation}) {
+  function manageSearchResult(queryRes) {
+    if (queryRes.length === 0) return [];
+    const result = queryRes.map((place) => {
+      // Place Obj Data Template
+      const placeObj = {
+        name: place.label,
+        coord: {
+          lat: place.latitude,
+          long: place.longitude,
+        },
+        country: {
+          name: place.country,
+          region: place.region,
+        },
+        timezone: {
+          ...place.timezone_module,
+          moment: moment.tz(place.timezone_module.name),
+        },
+      };
 
+      return placeObj;
+    });
 
-    const searchQuery = useRef();
-    const [searchResult,setSearchResult] = useState([]);
+    // console.log(result,'result');
+    setSearchResult(result);
+  }
 
-    const [searchParam,setSearchParam] = useState('');
-    const {isLoading,refetch,isError,error} = useQuery(['searchRes',searchParam], fetchLocation,{enabled:false,onSuccess:(data)=>manageSearchResult(data.data)});
-
-    // Fetch the API for location coordinate
-    async function searchLocation(e){
-
-        // Validation And Initialization
-        e.preventDefault();
-        const query = searchQuery.current.value;
-        setSearchResult([]);
-        if( query === "") return;
-        await setSearchParam(query);
-        refetch()
-        // if(queryRes.status === 200){
-        //     console.log(queryRes.data);
-        //     // manageSearchResult(queryRes.data.data);
-        // }else{
-        //     //Show Error
-        // }
-    }
-    async function fetchLocation(queryKey,id){
-         
-        // console.log('ID:',id);
-        console.log('QK:',queryKey.queryKey[1]);
-        // Construc the endpoint
-        const url = new URL('https://toki-timer-be.vercel.app/search');
-        url.searchParams.append('name',searchParam);
-        const queryRes = await axios.get(url);
-        return queryRes.data;
-    }
-
-    function manageSearchResult(queryRes){
-
-        if(queryRes.length === 0) return [];
-        const result = queryRes.map((place)=>{
-
-            // Place Obj Data Template
-            const placeObj = {
-                name:place.label,
-                coord:{
-                    lat:place.latitude,
-                    long:place.longitude
-                },
-                country:{
-                    name:place.country,
-                    region:place.region
-                },
-                timezone:{
-                    ...place.timezone_module,
-                    moment:moment.tz(place.timezone_module.name)
-                }
-
-            }
-
-
-            return placeObj;
-        });
-
-        // console.log(result,'result');
-        setSearchResult(result);
-    }
-
-    function handleAdd(place){
-        addLocation(place);
-    }
-    function renderSearchResult(searchResult){
-        return searchResult.map((placeObj,index)=>{
-                return (
-                    <div className="location-res" key={index}>
-                       <div className="location-title">
-                            <h2>{placeObj.name}</h2>
-                            <hr />
-                       </div>
-                        <div className="timezone">
-                            <p className="detail_coord">{placeObj.coord.lat} & {placeObj.coord.long} </p>
-                            <p className="detail_date">{placeObj.timezone.moment.format('h:m A - D MMMM YYYY ')}</p>
-                            {/* <h3>Timezone</h3> */}
-                            <div className="detail_timezone">
-                                <p>{placeObj.timezone.name}</p>
-                                <p>UTC{placeObj.timezone.offset_string}</p>
-                            </div>
-                        </div>
-                        <div className="action">
-                            <StyledButton size={'.7rem'} onClick={()=>{handleAdd(placeObj)}}>Add  <FaArrowRight/> </StyledButton>
-                        </div>
-                    </div>
-                )
-            });
-    }
-    return (
-        <StyledLocationSearch>
-            <h2> <TiLocation/> Location Search</h2>
-            <StyledModalForm onSubmit={searchLocation}>
-                <div className="stack">
-                    <input type="search" ref={searchQuery} onChange={(e)=>{if(e.target.value === "")setSearchResult([]);}} placeholder="Search a location!" />
-                    <StyledButton type="submit" style={{height:'fit-content',alignSelf:'center'}}><FaSearchLocation/></StyledButton>
-                </div>
-            </StyledModalForm>
-
-            <div className="search-res">
-                {isLoading && 
-                <>
-                    <p style={{opacity:'.5'}}>Finding Location . . .</p>
-                </>}
-
-                {isError && <p>{error}</p>}
-                {searchResult && renderSearchResult(searchResult)}
+  function handleAdd(place) {
+    addLocation(place);
+  }
+  function renderSearchResult(searchResult) {
+    return searchResult.map((placeObj, index) => {
+      return (
+        <div className="location-res" key={index}>
+          <div className="location-title">
+            <h2>{placeObj.name}</h2>
+            <hr />
+          </div>
+          <div className="timezone">
+            <p className="detail_coord">
+              {placeObj.coord.lat} & {placeObj.coord.long}{" "}
+            </p>
+            <p className="detail_date">
+              {placeObj.timezone.moment.format("h:m A - D MMMM YYYY ")}
+            </p>
+            {/* <h3>Timezone</h3> */}
+            <div className="detail_timezone">
+              <p>{placeObj.timezone.name}</p>
+              <p>UTC{placeObj.timezone.offset_string}</p>
             </div>
-        </StyledLocationSearch>
-    )
+          </div>
+          <div className="action">
+            <StyledButton
+              size={".7rem"}
+              onClick={() => {
+                handleAdd(placeObj);
+              }}
+            >
+              Add <FaArrowRight />{" "}
+            </StyledButton>
+          </div>
+        </div>
+      );
+    });
+  }
+  return (
+    <StyledLocationSearch>
+      <h2>
+        {" "}
+        <TiLocation /> Location Search
+      </h2>
+      <StyledModalForm onSubmit={searchLocation}>
+        <div className="stack">
+          <input
+            type="search"
+            ref={searchQuery}
+            onChange={(e) => {
+              if (e.target.value === "") setSearchResult([]);
+            }}
+            placeholder="Search a location!"
+          />
+          <StyledButton
+            type="submit"
+            style={{ height: "fit-content", alignSelf: "center" }}
+          >
+            <FaSearchLocation />
+          </StyledButton>
+        </div>
+      </StyledModalForm>
+
+      <div className="search-res">
+        {isLoading && (
+          <>
+            <p style={{ opacity: ".5" }}>Finding Location . . .</p>
+          </>
+        )}
+
+        {isError && <p>{error}</p>}
+        {searchResult && renderSearchResult(searchResult)}
+      </div>
+    </StyledLocationSearch>
+  );
 }
--- src/components/Timezone.jsx
+++ src/components/Timezone.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import DigitalTime from "./DigitalTimer";
 import { dateToTime } from "../utility";
 
@@ -9,13 +9,11 @@ import styled from "styled-components";
 import axios from "axios";
 import moment from "moment-timezone";
 
-
-import {BiCurrentLocation} from 'react-icons/bi'
-import {CgEditBlackPoint} from 'react-icons/cg'
+import { BiCurrentLocation } from "react-icons/bi";
+import { CgEditBlackPoint } from "react-icons/cg";
 import { useQuery } from "react-query";
 
 const StyledTimezone = styled.div`
-  
   /* max-width: 1200px; */
   width: 80vw;
   height: 65vh;
@@ -23,218 +21,239 @@ const StyledTimezone = styled.div`
   overflow-y: hidden;
   padding-bottom: 4em;
 
-  & .tz-content{
+  & .tz-content {
     /* display: grid;  */
     /* grid-template-columns: 300px 300px; */
     display: flex;
     justify-content: left;
     white-space: nowrap;
     gap: 1em;
-    width:100%;
+    width: 100%;
     max-height: 100%;
   }
 
-  & .wide > div{
+  & .wide > div {
     max-height: 100%;
   }
 
-  & .search > div{
+  & .search > div {
     max-height: 100%;
   }
 
-  & > h2{
-    font-size:1.5rem;
+  & > h2 {
+    font-size: 1.5rem;
     color: #534e4fa5;
     font-weight: 400;
   }
-`
+`;
 
 const StyledLocalLocation = styled.div`
-    resize: horizontal;
-    overflow: auto;
-    height: 58vh;
-    background-color: #f7dee9a0;
-    padding: 2em;
-    border-radius: 10px;
-    min-width: 340px;
-    font-size: small;
+  resize: horizontal;
+  overflow: auto;
+  height: 58vh;
+  background-color: #f7dee9a0;
+  padding: 2em;
+  border-radius: 10px;
+  min-width: 340px;
+  font-size: small;
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+
+  h2 {
+    color: var(--black);
+  }
+  & .local-current {
     display: flex;
     flex-direction: column;
-    max-width: 500px;
+    align-items: center;
+    justify-content: center;
+    height: 50vh;
+  }
+  & .location {
+    /* display: flex; */
+    /* flex-direction: column; */
+    /* justify-content: center; */
+    /* height: 100%; */
+  }
+  & .location .time {
+    font-size: 1.4rem;
+  }
+  & .location .place-name {
+    font-size: 1rem;
+  }
+  & .coord {
+    display: flex;
+    justify-content: space-between;
 
-    h2{
-        color:var(--black);
-    }
-    & .local-current{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 50vh;
-    }
-    & .location{
-        /* display: flex; */
-        /* flex-direction: column; */
-        /* justify-content: center; */
-        /* height: 100%; */
-    }
-    & .location .time{
-        font-size: 1.4rem;
-    }
-    & .location .place-name{
-        font-size: 1rem;
-    }
-    & .coord{
-        display: flex;
-        justify-content: space-between;
-
-        background-color: #fb87b929;
-        margin: .2em;
-        padding: .5em 2em;
-        text-align:center;
-    }
-    & .coord h2{
-        font-size:1rem;
-        /* color: rgb(221, 137, 156); */
-        color: rgba(119, 122, 197, 0.584);
-        font-weight: normal;
-        
-    }
-    & .coord p{
-        opacity: .8;
-        color: rgb(148, 102, 161);
-    }
+    background-color: #fb87b929;
+    margin: 0.2em;
+    padding: 0.5em 2em;
+    text-align: center;
+  }
+  & .coord h2 {
+    font-size: 1rem;
+    /* color: rgb(221, 137, 156); */
+    color: rgba(119, 122, 197, 0.584);
+    font-weight: normal;
+  }
+  & .coord p {
+    opacity: 0.8;
+    color: rgb(148, 102, 161);
+  }
 `;
 
 export default function Timezone() {
-           
-    const [currentLocation,setCurrentLocation] = useState();
-    const [localLocation,setLocalLocation] = useState({});
-    const [savedLocation,setSavedLocation] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState();
+  const [localLocation, setLocalLocation] = useState({});
+  const [savedLocation, setSavedLocation] = useState(
+    JSON.parse(localStorage.getItem("saved-location")) || []
+  );
 
-    useEffect(()=>{
-        navigator.geolocation.getCurrentPosition((pos)=>{
-            setCurrentLocation(pos);
-            searchLocalPosition(pos.coords.latitude,pos.coords.longitude);
-        });
-    },[]);
-    const today = new Date();
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setCurrentLocation(pos);
+      searchLocalPosition(pos.coords.latitude, pos.coords.longitude);
+    });
+  }, []);
+  const today = new Date();
 
-
-    function handleAddLocation(place){
-        const hasBeenAddedBefore = savedLocation.find((val)=> _.isEqual(val.coord,place.coord));
-
-        // If there is no place in the list that has the same exact coordinates
-        if(!hasBeenAddedBefore){
-            setSavedLocation(prev=>{
-                return [...prev,place];
-            })
-            console.log('added',place);
-        }
+  function handleAddLocation(place) {
+    const hasBeenAddedBefore = savedLocation.find((val) =>
+      _.isEqual(val.coord, place.coord)
+    );
+    // If there is no place in the list that has the same exact coordinates
+    if (!hasBeenAddedBefore) {
+      setSavedLocation((prev) => {
+        const mutatedData = [...prev, place];
+        localStorage.setItem("saved-location", JSON.stringify(mutatedData));
+        return mutatedData;
+      });
+      console.log("added", place);
     }
+  }
 
-    // Fetch the API for location coordinate
-    async function searchLocalPosition(lat,long){
+  // Fetch the API for location coordinate
+  async function searchLocalPosition(lat, long) {
+    const url = new URL("https://toki-timer-be.vercel.app/home");
+    const coord = lat + "," + long;
+    url.searchParams.append("lat", lat);
+    url.searchParams.append("long", long);
 
-    
-        const url = new URL('https://toki-timer-be.vercel.app/home');
-        const coord = lat + ',' + long;
-        url.searchParams.append("lat",lat);
-        url.searchParams.append("long",long);
-        
-        // Fetch Data 
-        const queryRes = await axios.get(url);
-        if(queryRes.status === 200){
-            const data = convertToPlaceObj(queryRes.data.data[0]);
-            setLocalLocation(data);
-            console.log(data);
-        }else{
-            //Show Error
-            setLocalLocation({message:"Couldn't find the local location from the timezone database!"});
-        }
+    // Fetch Data
+    const queryRes = await axios.get(url);
+    if (queryRes.status === 200) {
+      const data = convertToPlaceObj(queryRes.data.data[0]);
+      setLocalLocation(data);
+      console.log(data);
+    } else {
+      //Show Error
+      setLocalLocation({
+        message: "Couldn't find the local location from the timezone database!",
+      });
     }
+  }
 
-    function convertToPlaceObj(place){
-        const placeObj = {
-            name:place.label,
-            coord:{
-                lat:place.latitude,
-                long:place.longitude
-            },
-            country:{
-                name:place.country,
-                region:place.region
-            },
-            timezone:{
-                ...place.timezone_module,
-                moment:moment.tz(place.timezone_module.name)
-            }
+  function convertToPlaceObj(place) {
+    const placeObj = {
+      name: place.label,
+      coord: {
+        lat: place.latitude,
+        long: place.longitude,
+      },
+      country: {
+        name: place.country,
+        region: place.region,
+      },
+      timezone: {
+        ...place.timezone_module,
+        moment: moment.tz(place.timezone_module.name),
+      },
+    };
+    return placeObj;
+  }
 
-        }
-        return placeObj;
-    }
-
-    function handleRemoveLocation(name){
-        // console.log(name);
-        // return;
-        setSavedLocation((prev)=>{
-            const without = prev.filter((place)=> place.name !== name);
-            return without;
-        })
-    }
-    return (
-        <StyledTimezone>
-            <h2>Timezone</h2>
-            <div className="tz-content">
-                {currentLocation && (
-                        <StyledLocalLocation >
-                            <div className="local-current">
-                                <h2 style={{opacity:'.8'}}><BiCurrentLocation/> Current Location:</h2>
-                                {localLocation.name ? <Location className={'localLocation'} withDate={true} place={localLocation}/> : <p>{localLocation.message}</p>}
-                            </div>
-                            <div className="local-coords">
-                                <h2 style={{opacity:'.5',fontWeight:'normal',textAlign:'center'}}> <CgEditBlackPoint/> Coordinates:</h2>
-                                <div className="coord">
-                                    <div className="lat">
-                                        <h2>Lat</h2>
-                                         <p>{currentLocation.coords.latitude}</p>
-                                    </div>
-                                    <div className="long">
-                                        <h2>Long</h2>
-                                        <p>{currentLocation.coords.longitude}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </StyledLocalLocation>
-                    )}
-                <LocationSearch addLocation={handleAddLocation}></LocationSearch>
-                <div>
-                    <LocationList listOnClick={handleRemoveLocation} list={savedLocation}/>
-                </div>
-               
+  function handleRemoveLocation(name) {
+    // console.log(name);
+    // return;
+    setSavedLocation((prev) => {
+      const without = prev.filter((place) => place.name !== name);
+      return without;
+    });
+  }
+  return (
+    <StyledTimezone>
+      <h2>Timezone</h2>
+      <div className="tz-content">
+        {currentLocation && (
+          <StyledLocalLocation>
+            <div className="local-current">
+              <h2 style={{ opacity: ".8" }}>
+                <BiCurrentLocation /> Current Location:
+              </h2>
+              {localLocation.name ? (
+                <Location
+                  className={"localLocation"}
+                  withDate={true}
+                  place={localLocation}
+                />
+              ) : (
+                <p>{localLocation.message}</p>
+              )}
             </div>
-        </StyledTimezone>
-    )
+            <div className="local-coords">
+              <h2
+                style={{
+                  opacity: ".5",
+                  fontWeight: "normal",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                <CgEditBlackPoint /> Coordinates:
+              </h2>
+              <div className="coord">
+                <div className="lat">
+                  <h2>Lat</h2>
+                  <p>{currentLocation.coords.latitude}</p>
+                </div>
+                <div className="long">
+                  <h2>Long</h2>
+                  <p>{currentLocation.coords.longitude}</p>
+                </div>
+              </div>
+            </div>
+          </StyledLocalLocation>
+        )}
+        <LocationSearch addLocation={handleAddLocation}></LocationSearch>
+        <div>
+          <LocationList
+            listOnClick={handleRemoveLocation}
+            list={savedLocation}
+          />
+        </div>
+      </div>
+    </StyledTimezone>
+  );
 }
 
 // Deprecated
-function getLongitudeTime(long){
-    const hourDelay = long / 15;
-    const minuteDelay = hourDelay * 60;
-    const secondDelay = minuteDelay * 60;
+function getLongitudeTime(long) {
+  const hourDelay = long / 15;
+  const minuteDelay = hourDelay * 60;
+  const secondDelay = minuteDelay * 60;
 
+  // Convert to miliseconds
+  const miliHour = Math.floor(hourDelay) * 60 * 60;
+  const miliMinute = Math.floor(minuteDelay) * 60;
+  const miliSecond = Math.floor(secondDelay);
 
-    // Convert to miliseconds
-    const miliHour = Math.floor(hourDelay) * 60 * 60 ; 
-    const miliMinute = Math.floor(minuteDelay) * 60 ;
-    const miliSecond = Math.floor(secondDelay);
+  const miliDelay = miliHour + miliMinute + miliSecond;
+  const stringRes = `${Math.floor(hourDelay)}:${Math.floor(
+    minuteDelay
+  )}:${Math.floor(secondDelay)}`;
 
-
-    const miliDelay = miliHour + miliMinute + miliSecond;
-    const stringRes = `${Math.floor(hourDelay)}:${Math.floor(minuteDelay)}:${Math.floor(secondDelay)}`;
-    
-    const dateNow = new Date();
-    const dateLong = new Date(dateNow.getTime() + miliDelay);
-    return dateLong;
-
+  const dateNow = new Date();
+  const dateLong = new Date(dateNow.getTime() + miliDelay);
+  return dateLong;
 }
